9c658eb27c3d32b580f41cfb622fec5d73f836d1
Alex <alexpark@pursuit.org>
2019-06-12 12:16:33 -0400

    resume upload

--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { NavLink, withRouter } from 'react-router-dom'
 import Contacts from './contacts'
 
+import Resume from '../../Resume.pdf'
 import './navbar.css'
 
 class NavBar extends Component {
@@ -25,7 +26,7 @@ class NavBar extends Component {
                     <span className={`bottomBorder1 ${path === '/about' ? 'active' : ''}`}></span>
                     <NavLink className='projects link' to={'/projects'}>Projects</NavLink>
                     <span className={`bottomBorder2 ${path === '/projects' ? 'active' : ''}`}></span>
-                    <NavLink className='resume link' to={'/resume'}>Resume</NavLink>
+                    <a className='resume link' href={Resume} target='_blank'>Resume</a>
                     <span className={`bottomBorder3 ${path === '/resume' ? 'active' : ''}`}></span>
                 </div>
                 <Contacts/>
